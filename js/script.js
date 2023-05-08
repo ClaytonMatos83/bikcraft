5e8e4db91b8e836f49f554cccb48d649ea0166e6
ClaytonMatos83 <clayton_matos@outlook.com.br>
2023-05-08 13:32:30 -0300

    url parameters

--- js/script.js
+++ js/script.js
@@ -11,7 +11,20 @@ function ativarLink(link) {
 
 }
 
-
 links.forEach(ativarLink);
+
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+ const elemento = document.getElementById(parametro);
+ if (elemento) {
+  elemento.checked = true;
+ }
+}
+
+parametros.forEach(ativarProduto);
+
+
 
 
